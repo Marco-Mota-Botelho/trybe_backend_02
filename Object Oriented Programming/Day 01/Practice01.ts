e1ec4b62a215a02f0b28f85d8ff9f5f0f7d37ba3
Marco <marcobotelho93@gmail.com>
2023-01-11 13:44:19 -0300

    Exercise 2, worked on encapsulating my code and created a getter and a setter.

--- Object Oriented Programming/Day 01/Practice01.ts
+++ Object Oriented Programming/Day 01/Practice01.ts
@@ -1,23 +1,36 @@
 class Television {
-  brand: string;
+  readonly brand: string;
   size: number;
   resolution: string;
-  connections: Array<string>;
-  connectedTo: string;
+  private _connections: Array<string>;
+  private _connectedTo?: string;
 
   constructor (brand: string, size: number, resolution: string, connections: Array<string>) {
     this.brand = brand;
     this.size = size;
     this.resolution = resolution;
-    this.connections = connections;
+    this._connections = connections;
 
 
     console.log(`New ${ brand } television created.`);
   }
 
   turnOn():void {
-    console.log(this.brand, this.size, this.resolution, this.connections)
+    console.log(this.brand, this.size, this.resolution, this._connections)
   }
+
+  get connectedTo(): string | undefined {
+    return this._connectedTo
+  }
+
+  set connectedTo (connectionType: string | undefined) {
+    if(!connectionType || !this._connections.includes(connectionType) ) {
+      console.log('Sorry, connection unavailable');
+      return 
+    }
+    this._connectedTo = connectionType
+  }
+
 }
 
 const tv1 =  new Television('Samsung', 27, "4k", ["HDMI", "Ethernet", "DVI"])
